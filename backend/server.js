a17390ac2093a2d2052a97391b89071204f35a3f
Bryan <93453907+blopez024@users.noreply.github.com>
2025-06-10 20:38:06 -0700

    complete: chef marco wants the search

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,33 @@ import { menu } from './data.js';
 import express from 'express'; // Import express
 const app = express(); //Instantiate a new instance of express
 
-//Create a new endpoint on the root route
+// Create a new endpoint on the root route
 app.get('/', function (request, response) {
     // Send back to the client "Hello world"
     response.send('Hello world').end();
 });
 
+// Create a new endpoint for menu with pricing option
 app.get('/menu', (req, res) => {
-    res.send(menu);
+    const { maxPrice } = req.query;
+
+    // If maxPrice is not provided or invalid, return the full menu
+    if (isNaN(maxPrice)) {
+        return res.json(menu);
+    }
+
+    const filteredMenu = menu.filter((item) => item.price <= maxPrice);
+
+    if (filteredMenu.length === 0) {
+        return res.status(404).json({
+            error: `No menu items found with a price less than or equal to ${maxPrice}.`,
+        });
+    }
+
+    res.json(filteredMenu);
 });
 
+// Create a new endpoint to retrieve specific menu item
 app.get('/menu/:menuItem', (req, res) => {
     const { menuItem } = req.params;
     res.send(menu.filter((item) => item.id == menuItem));
